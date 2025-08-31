37d9db60b647ebe0e4f1d874614a3e6e76c84824
Cloud <44526554+Cloud7050@users.noreply.github.com>
2025-08-31 22:35:03 +0800

    Update link bat file

--- link.bat
+++ link.bat
@@ -1,6 +1,6 @@
-del "C:\Users\cloud\Documents\My Games\Path of Exile\Cloud.filter"
-del "C:\Users\cloud\Documents\My Games\Path of Exile\Test.filter"
-del "C:\Users\cloud\Documents\My Games\Path of Exile\WAH.mp3"
-mklink "C:\Users\cloud\Documents\My Games\Path of Exile\Cloud.filter" C:\Users\cloud\Documents\Archives\aCode\NodeJS\PoE\ItemFilter\build\Cloud.filter
-mklink "C:\Users\cloud\Documents\My Games\Path of Exile\Test.filter" C:\Users\cloud\Documents\Archives\aCode\NodeJS\PoE\ItemFilter\build\Test.filter
-mklink "C:\Users\cloud\Documents\My Games\Path of Exile\WAH.mp3" C:\Users\cloud\Documents\Archives\aCode\NodeJS\PoE\ItemFilter\resources\WAH.mp3
+del "C:\Users\cloud\Documents\My Games\Path of Exile 2\Cloud.filter"
+del "C:\Users\cloud\Documents\My Games\Path of Exile 2\Test.filter"
+del "C:\Users\cloud\Documents\My Games\Path of Exile 2\WAH.mp3"
+mklink "C:\Users\cloud\Documents\My Games\Path of Exile 2\Cloud.filter" C:\Users\cloud\Documents\Cache\aCode\NodeJS\PoE\ItemFilter\build\Cloud.filter
+mklink "C:\Users\cloud\Documents\My Games\Path of Exile 2\Test.filter" C:\Users\cloud\Documents\Cache\aCode\NodeJS\PoE\ItemFilter\build\Test.filter
+mklink "C:\Users\cloud\Documents\My Games\Path of Exile 2\WAH.mp3" C:\Users\cloud\Documents\Cache\aCode\NodeJS\PoE\ItemFilter\resources\WAH.mp3
