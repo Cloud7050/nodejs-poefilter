3a94c68c893d835d8233aed7b37b51ae43bb3396
Cloud <44526554+Cloud7050@users.noreply.github.com>
2025-09-29 16:21:09 +0800

    Add all normal soul cores by price

--- src/index.js
+++ src/index.js
@@ -522,6 +522,37 @@ filter.block((c, e) => {
 
 	e.colourAugment(SOCKETABLE_COLOUR, SOCKETABLE_PRESET).sizeAugment();
 });
+// Normal soul cores
+filter.block((c, e) => {
+	c.names = new Comparison(new StringList(
+		"Soul Core of Puhuarte", // 13 exalts
+		"Soul Core of Zalatl", // 18 exalts
+		"Soul Core of Opiloti", // 20 exalts
+		"Soul Core of Tzamoto", // 20 exalts
+		"Soul Core of Ticaba", // 20 exalts
+		"Soul Core of Quipolatl", // 22 exalts
+		"Soul Core of Citaqualotl", // 23 exalts
+		"Soul Core of Cholotl", // 23 exalts
+		"Soul Core of Atmohua", // 24 exalts
+		"Soul Core of Zantipi", // 24 exalts
+		"Soul Core of Topotante", // 25 exalts
+		"Soul Core of Xopec", // 26.92 exalts
+	));
+	c.category = new Comparison(CATEGORY.SOCKETABLE);
+
+	e.colourChance(SOCKETABLE_COLOUR, SOCKETABLE_PRESET).sizeChance();
+});
+filter.block((c, e) => {
+	c.names = new Comparison(new StringList(
+		"Soul Core of Azcapa", // 40 exalts
+		"Soul Core of Tacati", // 54 exalts
+		"Soul Core of Jiquani", // 3 divs
+	));
+	c.category = new Comparison(CATEGORY.SOCKETABLE);
+
+	e.colourDivine(SOCKETABLE_COLOUR, SOCKETABLE_PRESET).sizeChance();
+});
+//
 filter.block((c, e) => {
 	c.category = new Comparison(CATEGORY.SOCKETABLE);
 
