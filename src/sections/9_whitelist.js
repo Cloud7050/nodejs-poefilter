78a5c55a9e7bfa1f57b11b1555d1ddd0a9139bd1
Cloud <44526554+Cloud7050@users.noreply.github.com>
2025-10-10 15:34:00 +0800

    Whitelist all ilvl >= 82 items

--- src/sections/9_whitelist.js
+++ src/sections/9_whitelist.js
@@ -6,6 +6,8 @@ export function sectionWhitelist(filter) {
 		//TODO once encountered, highlight these differently. Could be 21-30% quality or extra socket
 		c.names = new Comparison("Exceptional ", OPERATOR.EQUAL);
 	}, (c) => {
+		c.ilvl = new Comparison(82, OPERATOR.GTE);
+	}, (c) => {
 		c.wisdomTier = new Comparison(4, OPERATOR.GTE);
 	}, (e) => {});
 }
